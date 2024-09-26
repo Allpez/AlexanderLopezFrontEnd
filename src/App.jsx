0603024066ab1df5b7616687c2d2e948a68c649d
Allpez <alexlop1987@gmail.com>
2024-09-26 01:28:57 -0500

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const routes = [
+  //   ["Home", "/"],
+  //   ["Projects", "/"],
+  //   ["Shop", "/"]
+  // ]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* <nav className='flex gap-3 sm:justify-center justify-end'>
+        {routes.map(([title, url]) => {
+          return <a className='bg-cyan-100 hover:bg-cyan-700 hover:text-white rounded-md py-1 px-2 my-2 border-solid border-2 border-cyan-700 hover:border-cyan-100' href={url}>{title}</a>
+        })}
+      </nav> */}
+      <Header></Header>
+      <div className='flex border-solid border-2 border-red-900 items-center mx-10 flex-wrap justify-center bg-white mb-6'>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
+function Cards() {
+  return (
+    <a href="#" className='w-80 m-5'>
+      <div className='flex flex-col bg-red-900 rounded-lg relative'>
+        <img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool.webp" alt="deadpool-img" className='object-cover w-full'></img>
+        <p className='mb-3 ms-14 text-white font-bold'>Pixel</p>
+        <div className='w-3 h-3 absolute bottom-0 right-0 bg-white rounded-br-md'></div>
+      </div>
+    </a>
+  )
+}
+
 export default App
+
+
+
+
+{/* <div>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_03.webp" alt="" ></img>
+<p className='ms-3'>Pixel</p>
+</div>
+
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_04.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_05.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_06.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_07.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_08.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_09.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_10.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_11.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_12.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_15.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_18.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_20.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_22.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_23.webp" alt="" ></img>
+<img src="https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_24.webp" alt="" ></img>
+ */}
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import { SearchBar } from "./SearchBar.jsx";
+
+function Header() {
+    return (
+      <>
+        <div className='flex flex-col sm:flex-row sm:justify-between m-10'>
+          <div className='text-center sm:text-start flex flex-col'>
+            <span className='text-2xl text-md font-bold text-red-500 mt-4'>DeadPool</span>
+            <h1 className='text-4xl text-white font-bold'>Discover the Multiverse</h1>
+          </div>
+          <SearchBar></SearchBar>
+        </div>
+      </>
+    )
+}
+
+export default Header;
