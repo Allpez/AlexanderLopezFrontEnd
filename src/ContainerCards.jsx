55e7b6c3c69541fbf7c276598c981cea59070ed4
Allpez <alexlop1987@gmail.com>
2024-09-26 19:28:22 -0500

    cards por objeto

--- src/ContainerCards.jsx
+++ src/ContainerCards.jsx
@@ -1,44 +1,39 @@
 function ContainerCards() {
-    const title = "pixel";
-    const img = [
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_03.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_04.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_05.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_06.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_07.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_08.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_09.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_10.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_11.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_12.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_15.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_18.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_20.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_22.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_23.webp",
-        "https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_24.webp"
+    const card = [
+        {title:"DeadPool", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool.webp"},
+        {title:"PixelPool", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_01.webp"},
+        {title:"Variant", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_02.webp"},
+        {title:"Deadpool Nerdy", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_03.webp"},
+        {title:"BabyPool", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_04.webp"},
+        {title:"Venomized", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_05.webp"},
+        {title:"Max Grecke", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_06.webp"},
+        {title:"Penny Arcade", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_07.webp"},
+        {title:"Skottie Young", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_08.webp"},
+        {title:"Post Apocalyptic", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_09.webp"},
+        {title:"Jim Lee", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_10.webp"},
+        {title:"Peach Momoko", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_11.webp"},
+        {title:"Midnight Suns", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_12.webp"},
+        {title:"Variant", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_18.webp"},
+        {title:"Pandart", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_20.webp"},
+        {title:"Headpool", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_22.webp"},
+        {title:"Dogpool", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_23.webp"},
+        {title:"Nicepool", img:"https://snapjson.untapped.gg/art/render/framebreak/common/512/Deadpool_24.webp"}
     ];
     return (
-        <div className="flex border-solid border-2 border-red-900 rounded-lg items-center mx-10 flex-wrap justify-center bg-white mb-6">
-            <DeadPoolCard title={title} image={img[0]}></DeadPoolCard>
-            <DeadPoolCard title={title} image={img[1]}></DeadPoolCard>
-            <DeadPoolCard title={title} image={img[2]}></DeadPoolCard>
-            <DeadPoolCard title={title} image={img[3]}></DeadPoolCard>
-            <DeadPoolCard title={title} image={img[0]}></DeadPoolCard>
-            <DeadPoolCard title={title} image={img[1]}></DeadPoolCard>
-            <DeadPoolCard title={title} image={img[2]}></DeadPoolCard>
-            <DeadPoolCard title={title} image={img[3]}></DeadPoolCard>
-
+        <div className="flex border-solid border-2 border-red-900 rounded-lg items-center mx-10 flex-wrap justify-center bg-white mb-6 p-3 py-12">
+            {card.map(c=>(
+                <DeadPoolCard title={c.title} image={c.img}></DeadPoolCard>
+            ))}
         </div>
     );
 }
 
-function DeadPoolCard (props) {
+function DeadPoolCard ({title, image}) {
     return (
         <a href="#" className='w-80 m-5'>
-            <div className='flex flex-col bg-red-900 border-black rounded-lg relative'>
-                <img src={props.image} alt={props.title} className='object-cover w-full'></img>
-                <p className='mb-3 ms-14 text-white font-bold'>{props.title}</p>
+            <div className='flex flex-col bg-red-900 border-black border-2 rounded-lg relative'>
+                <img src={image} alt={title} className='object-cover w-full'></img>
+                <p className='mb-3 ms-14 text-white font-bold'>{title}</p>
                 <div className='w-3 h-3 absolute bottom-0 right-0 bg-white rounded-br-md'></div>
             </div>
         </a>
--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -2,7 +2,7 @@ function SearchBar() {
     return (
       <>
         <div className="flex gap-1 justify-center my-6">
-          <input type="text" id="busqueda" className="border-2 border-red-800 rounded-lg ps-2 sm:h-9" placeholder="Search..."></input>
+          <input type="text" id="busqueda" className="border-2 min-w-80 border-red-800 rounded-lg ps-2 sm:h-9" placeholder="Search..."></input>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-white">
             <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
